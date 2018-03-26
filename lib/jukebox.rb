6d58125e738a2b77960d9a7daaa67128071607b2
derek chen <derek.g.chen@gmail.com>
2018-03-26 15:28:59 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,7 +13,15 @@ songs = [
 # #help
 #      lists out the possible commands (FAILED - 1)
 def help
-  
+puts "
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+
+
+"
 end
 
 # #play
